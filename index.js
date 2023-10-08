01086e41cd2455e7b286f9cdde9586a16020ca15
TVince0 <tvincedc@gmail.com>
2023-10-08 22:45:20 +0200

    Alap

---
+++ index.js
@@ -0,0 +1,9 @@
+function Fooldal(){
+    window.location.replace("./index.html");
+}
+function Letoltes(){
+    window.location.replace("./letoltes.html");
+}
+function Hasznalat(){
+    window.location.replace("./hasznalat.html");
+}
